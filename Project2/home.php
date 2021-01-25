78b630098d160ebad8d1f45cb21e4eb974ac48f3
abc <abc@example.com>
2021-01-25 18:07:20 +0530

    Project 2 updates

---
+++ Project2/home.php
@@ -0,0 +1,132 @@
+<!doctype html>
+<html lang="en">
+  <head>
+   
+   <meta charset="utf-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+   <link rel="stylesheet" type="text/css" href="styles.css"> 
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+   <link rel="preconnect" href="https://fonts.gstatic.com"> 
+   <link href="https://fonts.googleapis.com/css2?family=Balsamiq+Sans&display=swap" rel="stylesheet">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Chakra+Petch:wght@600&display=swap" rel="stylesheet">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Work+Sans&display=swap" rel="stylesheet">
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
+
+    <title>Off-Duty Freelancing Website</title>
+   
+  </head>
+  
+  <body style="background-color: PaleTurquoise;">
+
+
+
+ <header id="navbarheader" class="head" style="height: 85vh;">
+  <nav class="nbar">
+  <div class="logo">
+     <img src="logo.png" alt="" width="75" height="75">
+  
+</div>
+
+<div class="cname" style="font-family:'Lobster', cursive; font-size: 45px; text-shadow: 2px 2px 6px #000000;">
+  Off-Duty
+</div>
+
+  <div class="menu">
+    <ul>
+      <li >
+        <a  href="#home">Home</a>
+      </li>
+      <li >
+        <a  href="#aboutus" >Find Freelancers</a>
+      </li>
+     <li >
+        <a  href="#categories">Categories</a>
+      </li>
+      <li>
+        <a  href="#signup">Sign Up!</a>
+      </li>
+      <li>
+        <button class="btn btn-success" id="btn1" type="submit" style="background: rgb(148,222,238);background: linear-gradient(90deg, rgba(148,222,238,1) 0%, rgba(96,162,219,1) 27%, rgba(67,84,208,1) 63%, rgba(92,42,199,1) 100%);">Login</button>
+      </li>
+    </ul>
+      
+    </div>    
+  </nav>
+   <div class="join">   
+      <div class="vector">
+       <img src="vect1.png">
+    </div>
+        
+       <div class="content">
+        <h1 style="text-shadow: 2px 2px 6px #000000; font-family:'Lobster', cursive;">Off-Duty</h1>
+        <p style="text-shadow: 2px 2px 6px #000000;">
+          Utilize your free time to earn lumsum money. If your are a expert in your field then join today Off-Duty and be your own boss.
+        </p>
+        <div class="welcome">
+          <form action="">
+          <input id="input1" type="text" placeholder="I am looking for..." name="search">
+          <button class="btn btn-success" id="search" type="submit" style="background: rgb(148,222,238);background: linear-gradient(90deg, rgba(148,222,238,1) 0%, rgba(96,162,219,1) 27%, rgba(67,84,208,1) 63%, rgba(92,42,199,1) 100%);"> Search</button>
+          </form>
+       </div>
+       </div> 
+    </div>  
+    <div style="text-align: center;">
+    <i class="fa fa-angle-down" style="font-size:36px;color:white;"></i>
+    </div>
+  </header>
+
+
+
+
+<div class="container-fluid" style="text-align: center; margin-top: 35px; background: rgb(148,222,238);background: linear-gradient(90deg, rgba(148,222,238,1) 0%, rgba(96,162,219,1) 27%, rgba(67,84,208,1) 63%, rgba(92,42,199,1) 100%); color: white; margin-bottom: 0px; padding:20px 20px 10px 20px; ">
+	<h2 style="font-family:Chakra Petch', sans-serif;">Freelancing Services</h2> 
+</div>
+ 
+<div class="container-fluid" id="content2" style="display: flex; flex-direction: row; justify-content: space-around;  background: rgb(148,222,238);background: linear-gradient(90deg, rgba(148,222,238,1) 0%, rgba(96,162,219,1) 27%, rgba(67,84,208,1) 63%, rgba(92,42,199,1) 100%); height: 480px; padding: 0px 35px 35px 35px;">
+   <div id="para1" style="font-size:14px; width:25%; height: 360px;margin: 20px; background-image: url('bgpic1.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover; padding:20px; border-radius: 15px;box-shadow: 5px 10px 8px #888888;">
+    <h2 style="color: White; text-align: justify;font-size: 35px; text-shadow: 2px 2px 4px #000000;">Web Application Developer</h2>
+   </div>
+
+   <div id="para2" style="font-size:14px; width:25%; height: 360px;margin: 20px; background-image: url('bgpic2.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover; padding:20px; border-radius: 15px;box-shadow: 5px 10px 8px #888888;">
+     <h2 style="color: White; text-align: justify; text-shadow: 2px 2px 4px #000000;">UI/UX Designer</h2>
+   </div>
+
+   <div id="para3" style="font-size:14px; width:25%; height: 360px;margin: 20px; background-image: url('bgpic3.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover; padding:20px; border-radius: 15px;box-shadow: 5px 10px 8px #888888;">
+    <h2 style="color: White; font-size: 35px; text-shadow: 2px 2px 4px #000000;">Articial Intelligence Expert</h2>
+   </div>
+
+   <div id="para4" style="font-size:14px; width:25%; height: 360px;margin: 20px; background-image: url('bgpic4.jpg'); background-position: center; background-repeat: no-repeat; background-size: cover; padding:20px; border-radius: 15px;box-shadow: 5px 10px 8px #888888;">
+    <h2 style="color: White; font-size: 35px;text-shadow: 2px 2px 4px #000000;">Anroid App Developer</h2>
+   </div>
+  </div>
+
+
+ <div class="container-fluid">
+    
+   
+
+
+ </div>
+
+
+  <footer style="background-color: Lavender;height: 100px; margin-top: 50px; background:rgb(148,222,238);background: linear-gradient(90deg, rgba(148,222,238,1) 0%, rgba(96,162,219,1) 27%, rgba(67,84,208,1) 63%, rgba(92,42,199,1) 100%); ">
+    <div style="padding: 30px; text-align: center; color: white; text-shadow: 2px 2px 6px #000000;">
+      <h4 style="font-family:'Lobster', cursive;">Off-Duty | &#169; By MediCaps University, 2020 </h4>
+    </div>
+  </footer>
+
+
+
+    
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
